f7151d82cd194c1ab53fb51d97bc6d31eaca03d0
Wojciech Klause <wojtasxj@gmail.com>
2024-04-15 14:18:21 +0200

    task3 finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,60 @@
 class BooksList {
-    constructor() {
-        this.initData();
-        this.getElements();
-        this.initActions();
-    }
+  constructor() {
+    this.initData();
+    this.getElements();
+    this.render();
+    this.initActions();
+  }
 
-    initData() {
-        this.data = dataSource.books;
-    }
+  initData() {
+    this.data = dataSource.books;
+    this.favoriteBooks = [];
+  }
 
-    getElements() {
-        this.booksList = document.querySelector('.books-list');
-        this.source = document.getElementById('template-book').innerHTML;
-        this.template = Handlebars.compile(this.source);
-    }
+  getElements() {
+    this.booksList = document.querySelector('.books-list');
+    this.source = document.getElementById('template-book').innerHTML;
+    this.template = Handlebars.compile(this.source);
+  }
 
-    initActions() {
-        window.addEventListener('load', this.render.bind(this));
-    }
+  initActions() {
+    const bookImages = document.querySelectorAll('.books-list .book__image');
+    bookImages.forEach(bookImage => {
+      bookImage.addEventListener('dblclick', event => {
+        event.preventDefault();
+        const bookId = bookImage.dataset.id;
+        const isFavorite = this.favoriteBooks.includes(bookId);
+        if (isFavorite) {
+          const index = this.favoriteBooks.indexOf(bookId);
+          if (index !== -1) {
+            this.favoriteBooks.splice(index, 1);
+          }
+          bookImage.classList.remove('favorite');
+        } else {
+          this.favoriteBooks.push(bookId);
+          bookImage.classList.add('favorite');
+        }
+      });
+    });
+  }
 
-    filterBooks() {
-        // Metoda do filtrowania książek
-    }
+  filterBooks() {
+    // Metoda do filtrowania książek
+  }
 
-    determineRatingBgc() {
-        // Metoda do określania koloru tła na podstawie oceny
-    }
+  determineRatingBgc() {
+    // Metoda do określania koloru tła na podstawie oceny
+  }
 
-    render() {
-        this.data.forEach(book => {
-            const thisProduct = this;
-            const generatedHTML = template.menuProduct(thisProduct.data);
-            thisProduct.element = utils.createDOMFromHTML(generatedHTML);
-            const menuContainer = document.querySelector(select.containerOf.menu);
-            menuContainer.appendChild(thisProduct.element);
-        });
-    }
+  render() {
+    this.data.forEach(bookData => {
+      const generatedHTML = this.template(bookData);
+      const bookElement = utils.createDOMFromHTML(generatedHTML);
+      this.booksList.appendChild(bookElement);
+    });
+  }
 }
 
-const app = new BooksList();
+document.addEventListener('DOMContentLoaded', function() {
+  new BooksList();
+});
